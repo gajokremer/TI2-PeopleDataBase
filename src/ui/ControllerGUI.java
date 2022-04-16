95756178ca2d085cb2833bd7782c9fbc89457937
Gabriel Kremer <gajokremer@gmail.com>
2022-04-16 17:38:24 -0500

    Created Main and controller classes.

---
+++ src/ui/ControllerGUI.java
@@ -0,0 +1,13 @@
+package ui;
+
+import model.DataBase;
+
+public class ControllerGUI {
+
+    private DataBase dataBase;
+
+    public ControllerGUI() {
+
+        dataBase = new DataBase();
+    }
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,34 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private ControllerGUI controller;
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(""));
+        controller = new ControllerGUI();
+        fxmlLoader.setController(controller);
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("People Data Base");
+        primaryStage.show();
+
+//        controller.start();
+    }
+}
